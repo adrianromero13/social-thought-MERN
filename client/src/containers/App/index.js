d3b528e8b4952eede4fd05e00540c08289ed31b5
Adrian Romero <radrian031613@gmail.com>
2021-08-01 00:19:49 -0700

    new component set up to house Navigational Bar on top of application

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 // import components
+import Navigation from '../../components/Navigation';
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-// }));
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  display: {
+    margin: `${theme.spacing(1)}px auto`,
+    padding: theme.spacing(2),
+  }
+}));
 
 const App = () => {
-  // const classes = useStyles();
+  const classes = useStyles();
   return (
-    <Grid>
-      <Typography component='h1' variant='h6'>
-        Hello World!
-      </Typography>
-    </Grid>
+    <div className={classes.root}>
+          <Navigation />
+
+      <Grid className={classes.display}>
+          <Typography component='h1' variant='h6'>Application</Typography>
+      </Grid>
+    </div>
   )
 };
 
